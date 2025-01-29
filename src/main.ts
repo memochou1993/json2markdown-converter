27f7a44edf58f8e65b0f3af149b0d9f73c598a7e
Memo Chou <memochou1993@gmail.com>
2025-01-29 18:16:15 +0800

    Add markdown conversion features

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,97 @@
-import { setupCounter } from './counter.ts';
-import './style.css';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
+import { autocompletion, closeBrackets, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete';
+import { history, historyKeymap, indentWithTab, standardKeymap } from '@codemirror/commands';
+import { json } from '@codemirror/lang-json';
+import { bracketMatching, foldGutter, foldKeymap, indentOnInput } from '@codemirror/language';
+import { lintKeymap } from '@codemirror/lint';
+import { EditorState, Extension } from '@codemirror/state';
+import { EditorView, ViewUpdate, crosshairCursor, drawSelection, dropCursor, highlightActiveLine, highlightActiveLineGutter, highlightSpecialChars, keymap, lineNumbers, rectangularSelection } from '@codemirror/view';
+import { Converter as JSONToMarkdownConverter } from '@memochou1993/json2markdown';
+import { Converter as MarkdownToHTMLConverter } from '@memochou1993/markdown2html';
+import { toTitleCase } from '@memochou1993/stryle';
+import data from './data.json';
+import './style.scss';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+document.querySelector('#app')!.innerHTML = `
+  <header>
+    JSON2Markdown2HTML
+  </header>
+  <main>
+    <div class="container">
+      <div id="json-view"></div>
+      <div id="markdown-view" hidden></div>
+      <div id="html-view"></div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+  </main>
 `;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+const initEditor = (parent: Element, doc: string = '', extensions: Extension[] = []) => {
+  return new EditorView({
+    parent,
+    state: EditorState.create({
+      doc,
+      extensions: [
+        autocompletion(),
+        bracketMatching(),
+        closeBrackets(),
+        crosshairCursor(),
+        drawSelection(),
+        dropCursor(),
+        EditorState.allowMultipleSelections.of(true),
+        EditorView.contentAttributes.of({ 'aria-label': 'Editor' }),
+        foldGutter(),
+        highlightActiveLine(),
+        highlightActiveLineGutter(),
+        highlightSpecialChars(),
+        history(),
+        indentOnInput(),
+        keymap.of([
+          ...closeBracketsKeymap,
+          ...completionKeymap,
+          ...foldKeymap,
+          ...historyKeymap,
+          ...lintKeymap,
+          ...standardKeymap,
+          indentWithTab,
+        ]),
+        lineNumbers(),
+        rectangularSelection(),
+        ...extensions,
+      ],
+    }),
+  });
+};
+
+const handleUpdate = (view: EditorView, { state }: ViewUpdate) => {
+  let data = {};
+  try {
+    data = JSON.parse(state.doc.toString());
+  } catch {
+    return;
+  }
+  const doc = JSONToMarkdownConverter.toMarkdown(data, (element) => {
+    if (element.tag === 'heading') {
+      element.value = toTitleCase(element.value);
+    }
+    if (element.tag === 'tr') {
+      element.values = element.values.map(value => toTitleCase(String(value)));
+    }
+    return element;
+  });
+  view.dispatch({
+    changes: {
+      from: 0,
+      to: view.state.doc.length,
+      insert: doc,
+    },
+  });
+  document.querySelector('#html-view')!.innerHTML = MarkdownToHTMLConverter.toHTML(doc);
+};
+
+initEditor(document.querySelector('#json-view')!, JSON.stringify(data, null, 2), [
+  json(),
+  EditorView.updateListener.of(update => handleUpdate(markdownView, update)),
+]);
+
+const markdownView = initEditor(document.querySelector('#markdown-view')!, '', [
+  EditorState.readOnly.of(true),
+]);
